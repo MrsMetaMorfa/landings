4f7b05f6af00030421a64383a5f0cb9558fe3f1f
MrsMetaMorfa <maitressse.metamorfa@gmail.com>
2021-11-05 13:25:59 +0500

    Geodezis desktop layout

---
+++ assets/js/scripts.js
@@ -0,0 +1,13 @@
+/*!
+ * gulp-nunjucks-sass-template
+ * A Gulp 4 template including SCSS, Nunjucks, JS and more
+ *
+ * Url: https://github.com/DefaultSimon
+ * Author: Simon Goričar
+ * Copyright 2021. License: MIT
+ */
+window.onload = () => {
+  'use strict';
+
+}
+
